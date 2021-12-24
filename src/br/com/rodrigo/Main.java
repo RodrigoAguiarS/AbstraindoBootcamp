dbb4304d4558a2aad7912aba0241a69a27837898
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-12-24 08:46:28 -0300

    Initial commit

---
+++ src/br/com/rodrigo/Main.java
@@ -0,0 +1,24 @@
+package br.com.rodrigo;
+
+import br.com.rodrigo.dominio.Curso;
+import br.com.rodrigo.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("descrição curso Java");
+        curso.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(mentoria);
+
+    }
+}
